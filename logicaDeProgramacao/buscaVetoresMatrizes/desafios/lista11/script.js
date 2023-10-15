e100200677178e0d871fdbb143b2d3638a2d3a76
RubensLFerreira <rubenslima000@gmail.com>
2023-10-15 11:38:17 -0300

    exericio 11 - lista 01

--- logicaDeProgramacao/buscaVetoresMatrizes/desafios/lista11/script.js
+++ logicaDeProgramacao/buscaVetoresMatrizes/desafios/lista11/script.js
@@ -1,6 +1,23 @@
-function posicaoNaArvoreGenealogica(numero, letra) {
+const arvoreGenealogica = [
+  { geracao: -3, masculino: "bisavô", feminino: "bisavó" },
+  { geracao: -2, masculino: "avô", feminino: "avó" },
+  { geracao: -1, masculino: "pai", feminino: "mãe" },
+  {
+    geracao: 0,
+    masculino: "próprio individuo",
+    feminino: "próprio individuo",
+  },
+  { geracao: 1, masculino: "filho", feminino: "filha" },
+  { geracao: 2, masculino: "neto", feminino: "neta" },
+  { geracao: 3, masculino: "bisneto", feminino: "bisneta" },
+];
 
+function posicaoNaArvoreGenealogica(numero, letra) {
+  const linha = arvoreGenealogica.find((item) => item.geracao === numero);
+  const coluna = letra === "m" ? linha.masculino : linha.feminino;
+
+  return coluna;
 }
 
-const resultado = posicaoNaArvoreGenealogica(numero, letra);
+const resultado = posicaoNaArvoreGenealogica(-3, "m");
 console.log(resultado);
